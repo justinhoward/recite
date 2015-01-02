92d83a0bc2ca7b0e45ad517d1504d83ba60e5f01
Justin Howard <jmhoward0@gmail.com>
2015-01-02 13:40:57 -0800

    Added event tests
    Added tests for HttpRequestEvent and HttpResponseEvent. Added
    --recursive flag to mocha so tests in subdirectories will be run.

---
+++ test/events/httpResponseEventTest.js
@@ -0,0 +1,26 @@
+'use strict';
+var expect = require('chai').expect;
+var HttpResponseEvent = require('../../src/events/HttpResponseEvent');
+var Request = require('../../src/messages/Request');
+var Response = require('../../src/messages/Response');
+
+describe('events/HttpRequestEvent', function() {
+    it('can be constructed', function() {
+        var request = new Request('GET', '/test');
+        var response = new Response(request, 200);
+        var event = new HttpResponseEvent(response, {foo: 'foo val'});
+
+        expect(event.getResponse()).to.equal(response);
+        expect(event.get('foo')).to.equal('foo val');
+    });
+
+    it('can set a request', function() {
+        var request = new Request('GET', '/test');
+        var responseA = new Response(request, 200);
+        var responseB = new Response(request, 200);
+        var event = new HttpResponseEvent(responseA);
+        event.setResponse(responseB);
+
+        expect(event.getResponse()).to.equal(responseB);
+    });
+});
